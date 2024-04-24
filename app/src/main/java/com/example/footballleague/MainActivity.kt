b85cd3aae4619ff558fe5fb27a0f35c9f4e38b7c
Keshan <rambukkanage.keshan@gmail.com>
2024-04-24 14:57:02 +0530

    Made the buttons In main screen

--- app/src/main/java/com/example/footballleague/MainActivity.kt
+++ app/src/main/java/com/example/footballleague/MainActivity.kt
@@ -1,31 +1,77 @@
 package com.example.footballleague
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.footballleague.ui.theme.FootBallLeagueTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            FootBallLeagueTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+            MaterialTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Text(
+                            text = "Football Club Details",
+                            style = MaterialTheme.typography.headlineMedium,
+                            fontWeight = FontWeight.Bold ,
+                            modifier = Modifier.padding(bottom = 16.dp)
+
+
+                        )
+
+
+
+                        Button(
+                            onClick = { /*Intent(applicationContext,GuessTheCountry::class.java).also { startActivity(it) }*/ },
+                            modifier = Modifier.padding(12.dp).height(55.dp).width(230.dp),
+                            shape = RoundedCornerShape(topStart = 10.dp, bottomEnd = 10.dp),
+                        ) {
+                            Text(text = "Add Leagues to DB")
+                        }
+                        Button(
+                            onClick = { /*Intent(applicationContext,GuessHint::class.java).also { startActivity(it) } */},
+                            modifier = Modifier.padding(12.dp).height(55.dp).width(230.dp),
+                            shape = RoundedCornerShape(topStart = 10.dp, bottomEnd = 10.dp),
+                        ) {
+                            Text(text = "Search for Clubs By League")
+                        }
+                        Button(
+                            onClick = { /*Intent(applicationContext,GuessTheFlag::class.java).also { startActivity(it) }*/ },
+                            modifier = Modifier.padding(12.dp).height(55.dp).width(230.dp),
+                            shape = RoundedCornerShape(topStart = 10.dp, bottomEnd = 10.dp),
+                        ) {
+                            Text(text = "Search for Clubs")
+                        }
+
+                    }
                 }
             }
         }
+
     }
 }
 
